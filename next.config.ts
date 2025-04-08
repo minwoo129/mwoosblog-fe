e89dd60cb1dc062d9e2cbb653dbf2ac18921a534
minwoo129 <key0129mw@naver.com>
2025-04-08 14:00:39 +0900

    feat: #17 블로그 데이터 요청 작업 중간 저장

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
+import nextMDX from '@next/mdx';
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
