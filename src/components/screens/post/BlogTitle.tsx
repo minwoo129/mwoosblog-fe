ef02ba6cac772abf06f95ad38aeee8c98ca25ea8
minwoo129 <key0129mw@naver.com>
2025-04-09 13:28:33 +0900

    feat: #26 태그 영역 컴포넌트 구조 수정
    - 태그가 없을 땐 비노출 처리

--- src/components/screens/post/BlogTitle.tsx
+++ src/components/screens/post/BlogTitle.tsx
@@ -2,12 +2,22 @@ import dayjs from 'dayjs';
 import React from 'react';
 import ThumbnailImg from './ThumbnailImg';
 
-interface BlogTitleProps {
+interface BlogTitleProps extends TagsProps {
   title: string;
   createdAt: string;
   thumbnailImg: string;
   tags: string[];
 }
+
+interface TagsProps {
+  tags: string[];
+}
+
+const Tags = ({tags}: TagsProps) => {
+  if (tags.length === 0) return null;
+
+  return <p>({tags.map((tag, i) => `${i ? ', ' : ''}${tag}`)})</p>;
+};
 
 const BlogTitle = async ({
   title,
@@ -20,7 +30,7 @@ const BlogTitle = async ({
       <h1 className="mb-0 text-white">{title}</h1>
       <div className="flex gap-3 text-white">
         <p>{dayjs(createdAt).format('YYYY년 MM월 DD일')}</p>
-        <p>({tags.map((tag, i) => `${i ? ', ' : ''}${tag}`)})</p>
+        <Tags tags={tags} />
       </div>
       <hr className="mt-[8px]" />
       <ThumbnailImg title={title} thumbnailImg={thumbnailImg} />
